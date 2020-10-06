5fb695b8d80bcdd4316d4a0e6b64a76d80cc5769
Azes <34834132+Azes@users.noreply.github.com>
2020-10-06 20:37:10 +0200

    KI

---
+++ Assets/Enemy KI/enemytrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemytrigger : MonoBehaviour
+{
+
+    public simpleAI s;
+
+    private void Start()
+    {
+        s = transform.parent.GetComponent<simpleAI>();
+    }
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.CompareTag("Player"))
+        {
+            RaycastHit hit;
+
+            Vector3 dir = s.player.transform.position - transform.position;
+
+            if(Physics.Raycast(transform.position, dir, out hit))
+            {
+                if (hit.collider.CompareTag("Player"))
+                {
+                    Debug.Log("jo");
+                    s.isAtk = true;
+                }
+                else s.isAtk = false;
+            }
+
+           
+        }
+    }
+
+    private void OnTriggerExit(Collider c)
+    {
+       
+    }
+}
---
+++ Assets/Enemy KI/simpleAI.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simpleAI : MonoBehaviour
+{
+
+    public int Health = 100;
+    public bool isDead;
+    public float moveSpeed = 50;
+    public float aggroRange = 10;
+    public GameObject player;
+    public Rigidbody r;
+    public bool isAtk;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("FPSController");
+        r = GetComponent<Rigidbody>();
+    }
+
+   
+    public void addDamage(int damage)
+    {
+        Health -= damage; 
+
+        if(Health <= 0)
+        {
+            isDead = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isDead)
+        {
+
+
+
+            float dis = Vector3.Distance(transform.position, player.transform.position);
+            if(isAtk || dis <= aggroRange)
+            {
+                transform.LookAt(player.transform);
+
+                r.AddForce(transform.forward * moveSpeed * Time.deltaTime, ForceMode.Force);
+            }
+        }
+        else Destroy(gameObject);
+    }
+}
