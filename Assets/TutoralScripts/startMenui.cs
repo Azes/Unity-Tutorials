5e94ead1b78d6a7dd9b136064a8562190ba197fa
Azes <34834132+Azes@users.noreply.github.com>
2020-10-08 17:40:46 +0200

    new

---
+++ Assets/TutoralScripts/startMenui.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startMenui : MonoBehaviour
+{
+
+    public void startGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
